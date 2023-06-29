cbdd32637187c7fc6b7f1fd1c5e4b6a6584cce1b
Angel Legaspi <angel.lugia98@gmail.com>
2023-06-28 18:03:14 -0600

    bot ver

--- src/main/java/main/ConfigManager.java
+++ src/main/java/main/ConfigManager.java
@@ -7,7 +7,7 @@ import java.util.Properties;
 public class ConfigManager
 {
     private static final String PREFIX = "lb!";
-    private static final String VERSION = "v1.6.1";
+    private static final String VERSION = "v1.6.2";
     private static String token;
     private static String ownerId;
     private static String youtubeApiKey;
